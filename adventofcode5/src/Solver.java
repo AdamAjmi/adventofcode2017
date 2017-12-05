b3578424bda7a52e0683e5d3bd5e2ca7b551d29e
DESKTOP-EE03VG0\Adam <adam.ajmi@gmail.com>
2017-12-05 14:37:14 +0100

    Completed part 1, removed some test code. The way I exit the while loop with a try/catch is most certainly not ideal.

--- adventofcode5/src/Solver.java
+++ adventofcode5/src/Solver.java
@@ -1,59 +1,52 @@
-//import java.io.*;
-//import java.util.*;
+import java.io.*;
+import java.util.*;
 public class Solver{
 
-private int solve(int[] array) {
-    //jump denotes how many positions in array should be jumped
-    //pos is current "position" in the array
-    int i = 0;
-    int jump;
-    int pos = 0;
-    boolean b = true;
-
+    private int solve(int[] array) {
+        //jump denotes how many positions in array should be jumped
+        //pos is current position in the array
+        //i counts increments, in other words total jumps needed to exit the loop
+        int i = 0;
+        int jump;
+        int pos = 0;
+        boolean b = true;
 
         while (b) {
             //System.out.println(pos);
             //System.out.println(array.length);
             try {
-                if (pos > array.length || pos < 0) {
-                    b = false;
-                } else {
-                    jump = array[pos];
-                    //System.out.println("jump: " + jump);
-                    //System.out.println("value in array before: " + array[pos]);
-                    array[pos] = array[pos] + 1;
-                    //System.out.println("Value after increment +1: " + array[pos]);
+                jump = array[pos];
+                //System.out.println("value in array before: " + jump);
+                array[pos] = array[pos] + 1;
+                //System.out.println("Value after increment +1: " + array[pos]);
 
-                    pos = pos + jump;
-                    //System.out.println("Next position to jump to: " + pos);
-                    i++;
-                    //System.out.println("iteration nr: " + i);
-                }
+                pos = pos + jump;
+                //System.out.println("Next position to jump to: " + pos);
+                i++;
+                //System.out.println("iteration nr: " + i);
             }
             catch (ArrayIndexOutOfBoundsException ex) {
-                //System.out.println("reached catch");
+                System.out.println("reached catch");
                 break;
             }
         }
-    return i;
+        return i;
+    }
 
-}
-
-
-public static void main (String[] args) {
-        //Scanner s = new Scanner(new File("input.txt"));
-        //int[] array = new int[s.nextInt()];
-        //for (int i = 0; i < array.length; i++)
-        //array[i] = s.nextInt();
-
-        int array[] = new int[5];
-        array[0] = 0;
-        array[1] = 3;
-        array[2] = 0;
-        array[3] = 1;
-        array[4] = -3;
-
-        Solver solver = new Solver();
-        System.out.println(solver.solve(array));
+    public static void main (String[] args){
+        try {
+            int[] data = new int[1078];
+            Scanner s = new Scanner(new File("input.txt"));
+            int i = 0;
+            while(s.hasNextInt()){
+                data[i] = s.nextInt();
+                i++;
+            }
+            Solver solver = new Solver();
+            System.out.println(solver.solve(data));
         }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
